9bc7fd704c9dc973721b6cc74ca3ac7698233536
Egor Diomin <diomin.egor@gmail.com>
2023-08-08 01:43:12 +0300

    Completed Task08

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -3,3 +3,27 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Введите 3 числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    if (number1 > number3)
+    {
+        int result = number1;
+        Console.WriteLine($"Наибольшее число: {result}");
+    }
+}
+else if (number2 > number3)
+{
+    int result = number2;
+    Console.WriteLine($"Наибольшее число: {result}");
+}
+else
+{
+    int result = number3;
+    Console.WriteLine($"Наибольшее число: {result}");
+}
+
---
+++ Task08/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.Write("Введено неверное число!");
+}
+else if (number == 1)
+{
+    Console.WriteLine("В этом диапазоне четных чисел нет!");
+}
+else
+{
+    int count = 2;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count += 2;
+    }
+}
